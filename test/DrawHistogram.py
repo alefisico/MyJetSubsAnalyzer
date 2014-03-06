5db16cee8ac2d75466374fe9f7a83d2de21b0c0f
Alejandro Gomez Espinosa <alefisico@gmail.com>
2014-03-06 11:59:09 -0500

    including MCTruth Plots

--- test/DrawHistogram.py
+++ test/DrawHistogram.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #import ROOT
 from ROOT import * #TFile, TH1F, THStack, TCanvas, TMath, gROOT, gPad
-from setTDRStyle import setTDRStyle, setSelectionTitle
+from setTDRStyle import setTDRStyle, setSelectionTitle, setTitle
 import time, os, math
 import tarfile
 #import optparse
@@ -12,6 +12,9 @@ setTDRStyle()
 gROOT.ForceStyle()
 gROOT.SetStyle('tdrStyle')
 
+###############################################################
+### Dictionary with Histogram features                    #####
+###############################################################
 dictHisto = {}
 dictHisto[ 0 ]  = ['jetPt', 		0.,	500.,	100,	'p_{T} [GeV]', 			'Jets / 5 [GeV]']
 dictHisto[ 1 ]  = ['jetEta',		-5.,	5.,	100,	'jet #eta',			'Jets / 0.1']
@@ -55,140 +58,178 @@ dictHisto[ 38 ] = [ 'jet1MassPruned',	0.,	100.,	50,	'Leading Jet Pruned Invarian
 dictHisto[ 39 ] = [ 'jet2MassPruned',	0.,	100.,	50,	'2nd Leading Jet Pruned Invariant Mass [Gev]',	'Events / 2 [GeV]']
 dictHisto[ 40 ] = [ 'avgJetPt',      	0.,	200.,	40,	'p_{T}^{AVG} [GeV]',		'Events / 5 [GeV]']
 
-#fileNames = ['QCD500','QCD1000','RS2000','data']
-#xsections = [8426.,204.,4.083e-3,1.]
-#colorF    = [ROOT.kBlue-9,ROOT.kBlue-8,ROOT.kWhite,ROOT.kBlack]
-#colorL    = [ROOT.kBlack,ROOT.kBlack,ROOT.kRed,ROOT.kBlack]
-hist      = []
-#LUMI      = 19800.
-
-inputDir = '/cms/gomez/Stops/st_jj/treeResults/'
-outputDir = '/cms/gomez/Stops/st_jj/treeResults/Plots/'
-Sample = 'stopUDD312_50'
-jetAlgoList = [ 'CA8', 'AK4' ]
-
+###### Trick for add date or month to plots
 monthKey  =  time.strftime("%y%m")
 dateKey   = time.strftime("%y%m%d")
-targetDir = outputDir + dateKey + '/'
-tar = tarfile.open( targetDir+dateKey+"files.tar.gz", "w:gz")
-if not ( os.path.exists( targetDir ) ):
-	os.makedirs( targetDir )
 
-#---- open the files --------------------
-#for f in fileNames:
-inputFile = TFile.Open( inputDir+Sample+'_plots.root' )
-print inputFile.GetName()
+###############################################################
+###  Draw Histograms from final Tree (after selection)    #####
+###############################################################
+def analysisPlots():
+	"""create Plots from Final selection"""
 
-for jetAlgo in jetAlgoList:
-	for i, varList in dictHisto.iteritems():
+	Sample = 'stopUDD312_50'
+	inputDir = '/cms/gomez/Stops/st_jj/treeResults/'
+	outputDir = '/cms/gomez/Stops/st_jj/treeResults/Plots/'
+	jetAlgoList = [ 'CA8', 'AK4' ]
+
+	targetDir = outputDir + dateKey + '/'
+	print 'Output directory: ', targetDir
+	if not ( os.path.exists( targetDir ) ): os.makedirs( targetDir )
+	tarFile = dateKey+"files.tar.gz"
+	tar = tarfile.open( targetDir + tarFile, "w:gz")
+
+	outputList = [] ### for format
+
+	#---- open the files --------------------
+	inputFile = TFile.Open( inputDir+Sample+'_plots.root' )
+	print 'Drawing Histograms from: ', inputFile.GetName()
+
+	for jetAlgo in jetAlgoList:
+		for i, varList in dictHisto.iteritems():
+		
+			outputFileName = dateKey+'_'+varList[0]+'_'+jetAlgo+'.png'  
+			print 'Processing.......', outputFileName
+			outputFile = targetDir + outputFileName
+			outputList.append( outputFileName )
+			h = inputFile.Get('h_'+varList[0]+'_'+jetAlgo)
+			#h.Scale(wt)
+			#h.Rebin(rebin)
+			#h.SetDirectory(0)
+			#h.SetFillColor(colorF[i_f])
+			#h.SetLineColor(colorL[i_f])
+			#h.SetMarkerColor(colorL[i_f])
+			#hist.append(h)
+		   
+
+		#NQCD = hist[0].Integral()+hist[1].Integral()
+		#NDAT = hist[3].Integral()
+		#kFactor = NDAT/NQCD
+		#print kFactor
+		#
+		#hist[0].Scale(kFactor)
+		#hist[1].Scale(kFactor)
+		#
+		#histQCD = hist[0].Clone('histQCD')
+		#histQCD.Add(hist[1])
+		#
+		#hsQCD = THStack('QCD','QCD')
+		#
+		#hsQCD.Add(hist[0])
+		#hsQCD.Add(hist[1])
+
+			#----- Drawing -----------------------
+			h.GetXaxis().SetTitle( varList[4] )
+			h.GetYaxis().SetTitle( varList[5] )
+
+			can = TCanvas('can_'+str(i),'can_'+str(i),800,500)
+			h.Draw('HISTE')
+			setSelectionTitle( Sample )
+			can.SaveAs( outputFile )
+			del can
+		
+			tar.add( outputFile, dateKey+'_'+varList[0]+'_'+jetAlgo+'.png' )
+
+	tar.close()
+	print 'Creating tar file with all plots named: ',  tarFile
+	printHTML( outputList  )
+
+			#if logy: gPad.SetLogy()
+			#hAux = hist[3].Clone('aux')
+			#hAux.Reset()
+			#hAux.GetXaxis().SetRangeUser(xmin,xmax)
+			#hAux.GetXaxis().SetTitle(xtitle)
+			#hAux.SetMaximum(1.2*TMath.Max(hist[3].GetBinContent(hist[3].GetMaximumBin()),histQCD.GetBinContent(histQCD.GetMaximumBin())))
+			#hAux.SetMinimum(0.01)
+			#hAux.Draw()
+			#hsQCD.Draw('same hist')
+			#hist[3].Draw('same E')
+			#hist[2].Draw('same hist')
+			#gPad.RedrawAxis()
+		    
+###############################################################
+###  Draw Histograms from final Tree (after selection)    #####
+###############################################################
+def MCTruthAnalysis():
+	"""create Plots from MCTruth"""
+
+	Sample = 'stopUDD312_50'
+	inputDir = '/cms/gomez/Stops/st_jj/MCTruth/Plots/'
+	outputDir = '/cms/gomez/Stops/st_jj/MCTruth/Plots/'
+
+	targetDir = outputDir + dateKey + '/'
+	print 'Output directory: ', targetDir
+	if not ( os.path.exists( targetDir ) ): os.makedirs( targetDir )
+	tarFile = dateKey+"files.tar.gz"
+	tar = tarfile.open( targetDir + tarFile, "w:gz")
+
+	outputList = [] ### for format
+
+	dictMCHistos = {}
+	dictMCHistos[0]  = [ 'numberStops',		0.,	5.,	5, 	'Number of Stops (MCTruth)',		'Events / 1']
+	dictMCHistos[1]  = [ 'numberPartonStopA',	0.,	5.,	5, 	'Number of partons in StopA (MCTruth)',		'Events / 1']
+	dictMCHistos[2]  = [ 'numberPartonStopB',	0.,	5.,	5, 	'Number of partons in StopB (MCTruth)',		'Events / 1']
+	dictMCHistos[3]  = [ 'numberPartonsFromStop',	0.,	5.,	5, 	'Number of partons in Stops (MCTruth)',		'Events / 1']
+	dictMCHistos[4]  = [ 'ptStops', 		0.,	500.,	100,	'MCTruth Stops p_{T} [GeV]', 			'Stops / 5 [GeV]']
+	dictMCHistos[5]  = [ 'etaStops',		-5.,	5.,	100,	'MCTruth Stops #eta',			'Stops / 0.1']
+	dictMCHistos[6]  = [ 'phiStops',		-3.14,	3.14,	100, 	'MCTruth Stops #phi',			'Stops / 0.03']
+	dictMCHistos[7]  = [ 'massStops',		0.,	100.,	50,	'MCTruth Stops Invariant Mass [Gev]',	'Stops / 2 [GeV]']
+	dictMCHistos[8]  = [ 'invMassPartonsFromStop',		0.,	100.,	50,	'MCTruth Stops Reconstruct Invariant Mass [Gev]',	'Stops / 2 [GeV]']
+	dictMCHistos[9]  = [ 'ptPartonsFromStopA', 		0.,	500.,	100,	'MCTruth Partons from StopA p_{T} [GeV]',		'Stops / 5 [GeV]']
+	dictMCHistos[10] = [ 'etaPartonsFromStopA',		-5.,	5.,	100,	'MCTruth Partons from StopA #eta',			'Stops / 0.1']
+	dictMCHistos[11] = [ 'phiPartonsFromStopA',		-3.14,	3.14,	100, 	'MCTruth Partons from StopA #phi',			'Stops / 0.03']
+	dictMCHistos[12] = [ 'massPartonsFromStopA',		0.,	100.,	50,	'MCTruth Partons from StopA Invariant Mass [Gev]',	'Stops / 2 [GeV]']
+	dictMCHistos[9]  = [ 'ptPartonsFromStopB', 		0.,	500.,	100,	'MCTruth Partons from StopB p_{T} [GeV]',		'Stops / 5 [GeV]']
+	dictMCHistos[10] = [ 'etaPartonsFromStopB',		-5.,	5.,	100,	'MCTruth Partons from StopB #eta',			'Stops / 0.1']
+	dictMCHistos[11] = [ 'phiPartonsFromStopB',		-3.14,	3.14,	100, 	'MCTruth Partons from StopB #phi',			'Stops / 0.03']
+	dictMCHistos[12] = [ 'massPartonsFromStopB',		0.,	100.,	50,	'MCTruth Partons from StopB Invariant Mass [Gev]',	'Stops / 2 [GeV]']
+	dictMCHistos[9]  = [ 'ptTotalPartonsFromStop', 		0.,	500.,	100,	'MCTruth Partons from Stops p_{T} [GeV]',		'Stops / 5 [GeV]']
+	dictMCHistos[10] = [ 'etaTotalPartonsFromStop',		-5.,	5.,	100,	'MCTruth Partons from Stops #eta',			'Stops / 0.1']
+	dictMCHistos[11] = [ 'phiTotalPartonsFromStop',		-3.14,	3.14,	100, 	'MCTruth Partons from Stops #phi',			'Stops / 0.03']
+	dictMCHistos[12] = [ 'massTotalPartonsFromStop',	0.,	100.,	50,	'MCTruth Partons from Stops Invariant Mass [Gev]',	'Stops / 2 [GeV]']
+	#print dictMCHistos
+
+	#---- open the files --------------------
+	inputFile = TFile.Open( inputDir+Sample+'_plots.root' )
+	print 'Drawing Histograms from: ', inputFile.GetName()
+
+	for i, varList in dictMCHistos.iteritems():
 	
-		outputFile = targetDir+dateKey+'_'+varList[0]+'_'+jetAlgo+'.png'  
-		h = inputFile.Get('h_'+varList[0]+'_'+jetAlgo)
-		#h.Scale(wt)
-		#h.Rebin(rebin)
-		#h.SetDirectory(0)
-		#h.SetFillColor(colorF[i_f])
-		#h.SetLineColor(colorL[i_f])
-		#h.SetMarkerColor(colorL[i_f])
-		#hist.append(h)
-	   
-
-	#NQCD = hist[0].Integral()+hist[1].Integral()
-	#NDAT = hist[3].Integral()
-	#kFactor = NDAT/NQCD
-	#print kFactor
-	#
-	#hist[0].Scale(kFactor)
-	#hist[1].Scale(kFactor)
-	#
-	#histQCD = hist[0].Clone('histQCD')
-	#histQCD.Add(hist[1])
-	#
-	#hsQCD = THStack('QCD','QCD')
-	#
-	#hsQCD.Add(hist[0])
-	#hsQCD.Add(hist[1])
-
+		outputFileName = dateKey+'_'+varList[0]+'_'+Sample+'.png'  
+		print 'Processing.......', outputFileName
+		outputFile = targetDir + outputFileName
+		outputList.append( outputFileName )
+		h = inputFile.Get('h_'+varList[0])
 		#----- Drawing -----------------------
 		h.GetXaxis().SetTitle( varList[4] )
 		h.GetYaxis().SetTitle( varList[5] )
 
 		can = TCanvas('can_'+str(i),'can_'+str(i),800,500)
 		h.Draw('HISTE')
-		setSelectionTitle( Sample )
+		setTitle( Sample )
 		can.SaveAs( outputFile )
 		del can
-    	
-		tar.add( outputFile, dateKey+'_'+varList[0]+'_'+jetAlgo+'.png' )
+	
+		tar.add( outputFile, dateKey+'_'+varList[0]+'_'+Sample+'.png' )
 
-tar.close()
+	tar.close()
+	print 'Creating tar file with all plots named: ',  tarFile
+	printHTML( outputList  )
 
-		#if logy: gPad.SetLogy()
-		#hAux = hist[3].Clone('aux')
-		#hAux.Reset()
-		#hAux.GetXaxis().SetRangeUser(xmin,xmax)
-		#hAux.GetXaxis().SetTitle(xtitle)
-		#hAux.SetMaximum(1.2*TMath.Max(hist[3].GetBinContent(hist[3].GetMaximumBin()),histQCD.GetBinContent(histQCD.GetMaximumBin())))
-		#hAux.SetMinimum(0.01)
-		#hAux.Draw()
-		#hsQCD.Draw('same hist')
-		#hist[3].Draw('same E')
-		#hist[2].Draw('same hist')
-		#gPad.RedrawAxis()
-	    
-	#----- keep the GUI alive ------------
-	#if __name__ == '__main__':
-	#  rep = ''
-	#  while not rep in ['q','Q']:
-	#    rep = raw_input('enter "q" to quit: ')
-	#    if 1 < len(rep):
-	#      rep = rep[0]
+###############################################################
+###  Print histo name in HTML format                      #####
+###############################################################
+def printHTML( list ):
 
-	#usage = "usage: %prog [options]"
-	#parser = optparse.OptionParser(usage)
-	#parser.add_option("--var",action="store",type="string",dest="var",default='mjj')
-	#parser.add_option("--xmin",action="store",type="float",dest="xmin",default=1)
-	#parser.add_option("--xmax",action="store",type="float",dest="xmax",default=1)
-	#parser.add_option("--xtitle",action="store",type="string",dest="xtitle",default='')
-	#parser.add_option("--rebin",action="store",type="int",dest="rebin",default=1)
-	#parser.add_option("--logy",action="store_true",default=False,dest="logy")
-	#
-	#(options, args) = parser.parse_args()
-	#var = options.var
-	#xmin = options.xmin
-	#xmax = options.xmax
-	#xtitle = options.xtitle
-	#rebin = options.rebin
-	#logy = options.logy
-	#
+	print '#######################################################'
+	print '#### HTML code (for easy upload)                  #####'
+	print '#######################################################'
+	print "<ul>"
+	for histo in list: print "<li>", '<p><img src="%s"><br>' % histo
+	print "</ul>"
 
-def printHTML():
-	if (options.asList):
-	    print "<ul>"
-	for displayName, Tuple in filesMap.iteritems():
-	    sourceFile     = Tuple[0]
-	    fullTargetFile = Tuple[1]
-	    targetFile     = Tuple[2]
-	    if (options.asList):
-		print "   <li>",
-	    # Get the extention
-	    extMatch = re.search (r'\.([^\.]+)$', targetFile)
-	    printed = False
-	    if extMatch:
-		extention = extMatch.group(1).lower()
-		if extention in imageExtentions:
-		    print '<p><img src="%s"><br>' % targetFile
-		    printed = True
-		elif extention in convertedToImageExtentions:
-		    # Do we have a file of the same
-		    extReg = "%s%s" % (extention, "$")
-		    for imgExt in imageExtentions:
-			imageFile = re.sub (extReg, imgExt, displayName, re.IGNORECASE)
-			if imageFile in filesMap:
-			    print '<p><a href="%s"><img src="%s"></a><br>' % \
-				  (targetFile, imageFile)
-			    printed = True
-	    if not printed:
-		print '<a href="%s"><tt>%s</tt></a>' % (targetFile, displayName)
-	if (options.asList):
-	    print "</ul>"
+########################################################
+if __name__ == '__main__':
+
+	#analysisPlots()
+	MCTruthAnalysis()
